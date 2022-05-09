91540729298633de06b467fe6c4215d96c378d98
nagasaisriya <63009472+nagasaisriya@users.noreply.github.com>
2022-05-10 00:06:34 +0530

    Create SortColors.cpp

---
+++ SortColors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int m=0,l=0,h=nums.size()-1;
+        while(m<=h){
+            switch(nums[m]){
+                case 0:
+                    swap(nums[l++],nums[m++]);
+                    break;
+                case 1:
+                    m++;break;
+                case 2:
+                    swap(nums[m],nums[h--]);
+                    break;
+            }
+        }
+        
+    }
+};
